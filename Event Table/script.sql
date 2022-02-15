3abc02b1cd7eb593d60e9ec706c24e130ebc7613
CCNL-IT <98209853+CCNL-IT@users.noreply.github.com>
2022-02-15 10:01:37 +0800

    Add Fields
    Add lat field and lng field

--- Event Table/script.sql
+++ Event Table/script.sql
@@ -1,10 +1,3 @@
-USE [Dynamic]
-GO
-/****** Object:  Table [dbo].[Event]    Script Date: 01/25/2022 11:31:48 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
 CREATE TABLE [dbo].[Event](
 	[EventID] [uniqueidentifier] NOT NULL,
 	[ContainerID] [uniqueidentifier] NOT NULL,
@@ -18,15 +11,10 @@ CREATE TABLE [dbo].[Event](
 	[Voyage] [nvarchar](100) NULL,
 	[LocationPortCode] [nvarchar](50) NULL,
 	[LocationPort] [nvarchar](200) NULL,
+	[lat] [numeric](20, 16) NULL,
+	[lng] [numeric](20, 16) NULL,
  CONSTRAINT [PK_Event] PRIMARY KEY CLUSTERED 
 (
 	[EventID] ASC
 )WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
 ) ON [PRIMARY]
-GO
-/****** Object:  Default [DF_Event_EventID]    Script Date: 01/25/2022 11:31:48 ******/
-ALTER TABLE [dbo].[Event] ADD  CONSTRAINT [DF_Event_EventID]  DEFAULT (newid()) FOR [EventID]
-GO
-/****** Object:  Default [DF_Event_Actual]    Script Date: 01/25/2022 11:31:48 ******/
-ALTER TABLE [dbo].[Event] ADD  CONSTRAINT [DF_Event_Actual]  DEFAULT ((0)) FOR [Actual]
-GO
